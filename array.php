adce0227b3b053008baf54330ac4280e4472d2b3
irsyadhusni214 <irsyadaqiila27@gmail.com>
2019-08-03 16:07:08 +0700

    array boy

---
+++ array.php
@@ -0,0 +1,6 @@
+<?php
+    $a = [1 , 2, 4, 'Hello', 'kusnandar', 'susi', 'jono', 'kukus', 'juned', 'bambang', 100];
+    for ($i=0; $i < count($a); $i++) { 
+        echo 'Index array ke-' . $i .' Dengan isi data ' . $a[$i] . '<br>';
+    }
+?>
